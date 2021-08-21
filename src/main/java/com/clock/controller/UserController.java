dcbc98e49b70186d3c311b8f9cbac276c8d90e77
yuiyake <1306824899@qq.com>
2021-08-21 16:41:33 +0800

    '新增七牛云图片上传接口'

--- src/main/java/com/clock/controller/UserController.java
+++ src/main/java/com/clock/controller/UserController.java
@@ -8,13 +8,17 @@ import com.clock.service.UserService;
 import com.clock.util.ApiRes;
 import com.clock.bean.path;
 import com.clock.util.ImgRegulation;
+import com.clock.util.QiNiuUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -75,46 +79,64 @@ public class UserController {
         return userService.recoveryUser(id);
     }
 
+//    @RequestMapping("/imgStr")
+//    @ApiOperation("保存用户图片")
+//    public void imgStr(@RequestParam MultipartFile file, @RequestBody User user) throws IOException {
+//        if (file.equals("")){return;}
+//        Date date = new Date();
+//        String dateForm = new SimpleDateFormat("yyyy-MM-dd").format(date);
+//        String casePath = "C:\\Users\\Lenovo\\Desktop\\study\\java\\javaee\\大作业\\Clock-Project\\" +
+//                "SpringBoot-Clock-master\\src\\main\\java\\com\\clock\\util\\img"+dateForm;
+////        String casePath = path.getFileimg()+dateForm;
+//        String imgFormat = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+//
+////        简单验证图片
+//        ImgRegulation regulation = new ImgRegulation();
+//        try {
+//            boolean ifimg = regulation.VERIFY(imgFormat);
+//            if (false == ifimg)
+//                return;
+//        } catch (Exception e){
+//            return;
+//        }
+//
+//        //  判断文件是否存在
+//        File f = new File(casePath);
+//        try {
+//            if (!f.exists()){
+//                f.mkdirs();
+//            }
+//        }catch (Exception e){
+//            return;
+//        }
+//
+////        随机生成图片名字
+//        String name = UUID.randomUUID().toString()+imgFormat;
+//        file.transferTo(new File(casePath+"\\"+name));
+//        //拼接要保存在数据中的图片地址
+//        //dateForm 这是动态的文件夹所以要和地址一起存入数据库中
+//        //user 为@RequestMapping("/user")
+//        String urlImg = "http://localhost:8080/" + "user/show?fileUrl=" + dateForm + "/" + name;
+//        user.setUavg(urlImg);
+//        userService.setUserAvg(user);
+//    }
+
     @RequestMapping("/imgStr")
-    @ApiOperation("保存用户图片")
-    public void imgStr(@RequestParam MultipartFile file, @RequestBody User user) throws IOException {
-        if (file.equals("")){return;}
-        Date date = new Date();
-        String dateForm = new SimpleDateFormat("yyyy-MM-dd").format(date);
-        String casePath = "C:\\Users\\Lenovo\\Desktop\\study\\java\\javaee\\大作业\\Clock-Project\\" +
-                "SpringBoot-Clock-master\\src\\main\\java\\com\\clock\\util\\img"+dateForm;
-//        String casePath = path.getFileimg()+dateForm;
-        String imgFormat = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+    @ApiOperation("上传图片到七牛云")
+    public String imgStr(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
 
-//        简单验证图片
-        ImgRegulation regulation = new ImgRegulation();
-        try {
-            boolean ifimg = regulation.VERIFY(imgFormat);
-            if (false == ifimg)
-                return;
-        } catch (Exception e){
-            return;
+        // 用来获取其他参数
+        MultipartHttpServletRequest params = ((MultipartHttpServletRequest) request);
+        String name = params.getParameter("username");
+        if (!file.isEmpty()) {
+            FileInputStream inputStream = (FileInputStream) file.getInputStream();
+//            String path = QiNiuUtils.uploadQNImg(inputStream, PictureUtil.getRandomFileName()); // KeyUtil.genUniqueKey()生成图片的随机名
+            String path = QiNiuUtils.uploadQNImg(inputStream, file.getOriginalFilename());
+            System.out.print("七牛云返回的图片链接:" + path);
+            return path;
         }
+        return "上传失败";
 
-        //  判断文件是否存在
-        File f = new File(casePath);
-        try {
-            if (!f.exists()){
-                f.mkdirs();
-            }
-        }catch (Exception e){
-            return;
-        }
-
-//        随机生成图片名字
-        String name = UUID.randomUUID().toString()+imgFormat;
-        file.transferTo(new File(casePath+"\\"+name));
-        //拼接要保存在数据中的图片地址
-        //dateForm 这是动态的文件夹所以要和地址一起存入数据库中
-        //user 为@RequestMapping("/user")
-        String urlImg = "http://localhost:8080/" + "user/show?fileUrl=" + dateForm + "/" + name;
-        user.setUavg(urlImg);
-        userService.setUserAvg(user);
     }
 
 }
